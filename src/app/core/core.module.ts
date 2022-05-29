c5f5591525d22c61b013db9ce4382c64ab2d9307
Repuls1ve <repulsive@list.ru>
2022-05-29 18:30:46 +0300

    wip: #5 configure routing for different layouts

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { AngularSvgIconModule as SvgIconModule } from 'angular-svg-icon'
 import { CoreRoutingModule } from './core-routing.module'
 
 @NgModule({
-  imports: [CoreRoutingModule],
+  imports: [CoreRoutingModule, SvgIconModule.forRoot()],
   exports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, CoreRoutingModule]
 })
 export class CoreModule {}
