d95998444b082c7c732bd75b1bac828d32e0f4fd
Repuls1ve <repulsive@list.ru>
2022-05-29 20:07:50 +0300

    wip: #7 discoveries feature pages

---
+++ src/app/discoveries/feature/discovery-shell/discovery-shell.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core'
+import { DiscoveryShellRoutingModule } from './discovery-shell-routing.module'
+
+@NgModule({
+  imports: [DiscoveryShellRoutingModule]
+})
+export class DiscoveryShellModule {}
