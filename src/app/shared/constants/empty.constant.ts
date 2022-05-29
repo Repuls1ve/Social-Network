84c9dc8a2004b29ae0a3b793c1f14e324efc932b
Repuls1ve <repulsive@list.ru>
2022-05-29 18:28:01 +0300

    wip: #5 add query list constant

--- src/app/shared/constants/empty.constant.ts
+++ src/app/shared/constants/empty.constant.ts
@@ -1,3 +1,6 @@
+import { QueryList } from '@angular/core'
+
 export const EMPTY_STRING = ''
 export const EMPTY_ARRAY = []
 export const EMPTY_FUNCTION = () => {}
+export const EMPTY_QUERY = new QueryList<any>()
