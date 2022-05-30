ecb87eba6695d73bab609ce9d08edef2b2ab49c3
Repuls1ve <repulsive@list.ru>
2022-05-30 12:34:06 +0300

    feat: #9 button ui component

---
+++ src/app/shared/ui/button/button.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
+import { SpinnerModule } from '../spinner/spinner.module'
+import { ButtonComponent } from './button.component'
+
+@NgModule({
+  imports: [CommonModule, SpinnerModule],
+  declarations: [ButtonComponent],
+  exports: [ButtonComponent]
+})
+export class ButtonModule {}
