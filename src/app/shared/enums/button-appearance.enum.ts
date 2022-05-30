a891a1af9bd068e394d809c688025658b1ddcf48
Repuls1ve <repulsive@list.ru>
2022-05-30 12:33:16 +0300

    wip: #9 types for customizing button appearance

---
+++ src/app/shared/enums/button-appearance.enum.ts
@@ -0,0 +1,8 @@
+export enum ButtonAppearance {
+  Primary = 'primary',
+  Secondary = 'secondary',
+  Tertiary = 'tertiary',
+  Danger = 'danger'
+}
+
+export type ButtonAppearanceT = 'primary' | 'secondary' | 'tertiary' | 'danger'
