a80ce872defc0590e6eee0969a0b5b484ed6079a
Repuls1ve <repulsive@list.ru>
2022-05-30 12:38:06 +0300

    wip: #11 input type enum

---
+++ src/app/shared/enums/input-type.enum.ts
@@ -0,0 +1,46 @@
+export enum InputType {
+  Button = 'button',
+  Checkbox = 'checkbox',
+  Color = 'color',
+  Date = 'date',
+  DateTimeLocal = 'datetime-local',
+  Email = 'email',
+  File = 'file',
+  Hidden = 'hidden',
+  Month = 'month',
+  Number = 'number',
+  Password = 'password',
+  Radio = 'radio',
+  Range = 'range',
+  Reset = 'reset',
+  Search = 'search',
+  Submit = 'submit',
+  Tel = 'tel',
+  Text = 'text',
+  Time = 'time',
+  Url = 'url',
+  Week = 'week'
+}
+
+export type InputTypeT =
+  | 'button'
+  | 'checkbox'
+  | 'color'
+  | 'date'
+  | 'datetime-loca'
+  | 'email'
+  | 'file'
+  | 'hidden'
+  | 'month'
+  | 'number'
+  | 'password'
+  | 'radio'
+  | 'range'
+  | 'reset'
+  | 'search'
+  | 'submit'
+  | 'tel'
+  | 'text'
+  | 'time'
+  | 'url'
+  | 'week'
